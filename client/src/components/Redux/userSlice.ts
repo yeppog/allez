cf85908db9b3a1ced4787eec616ec1a4e64c3b09
Jonathan Lee <jon.lee.jing.yu@gmail.com>
2021-05-24 16:28:30 +0800

    client: feat: add redux for login

---
+++ client/src/components/Redux/userSlice.ts
@@ -0,0 +1,80 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from 'axios'
+import { LoginCredentials } from "../../interface/Credentials";
+
+interface UsersState {
+    user: string[]
+    status: 'idle' | 'pending' | 'succeeded' | 'failed'
+    error: string | null | undefined;
+}
+
+interface User {
+    username: string,
+    email: string,
+}
+
+const initialState = {
+    user: [],
+    status: 'idle',
+    error: null
+} as UsersState
+
+export const loginUser = createAsyncThunk('user/loginUser', async (body: LoginCredentials) => {
+    const credentials = body;
+    const res = await axios.post("http://localhost:3001/api/users/login", body, HTTPOptions); 
+    return res.data
+})
+
+export const checkLoggedInUser = createAsyncThunk('user/checkLoggedInUser', async () => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+        return { error: "User is not logged in."}
+    } else {
+        console.log(token);
+        const res = await axios.get("http://localhost:3001/api/users/verify", {headers: {...HTTPOptions.headers, "token": token }})
+    
+        return res.data;
+    }
+})
+
+
+const userSlice = createSlice({
+    name: 'user',
+    initialState,
+    reducers: {
+    },
+    extraReducers: (builder) => {
+        builder.addCase(loginUser.pending, (state, action) => {
+            state.status = 'pending';
+        })
+        builder.addCase(loginUser.fulfilled, (state, action) => {
+            state.status = 'succeeded';
+            state.user = state.user.concat(action.payload);
+            localStorage.setItem("token", action.payload.token);
+            console.log(localStorage.getItem("token"));
+        })
+        builder.addCase(loginUser.rejected, (state, action) => {
+            state.status = 'failed';
+            state.error = action.error.message;
+        })
+        builder.addCase(checkLoggedInUser.pending, (state, action) => {
+            state.status = 'pending';
+        })
+        builder.addCase(checkLoggedInUser.fulfilled, (state, action) => {
+            state.status = 'succeeded';
+            state.user = state.user.concat(action.payload);
+        })
+        builder.addCase(checkLoggedInUser.rejected, (state, action) => {
+            state.status = 'failed';
+            state.error = action.error.message;
+        })
+    }
+})
+
+const HTTPOptions = {
+    headers: {'Content-Type': 'application/json'}
+}
+
+export default userSlice.reducer;
+export const getUser = (state: UsersState) => state.user;
+export const getStatus = (state: UsersState) => state.status;
