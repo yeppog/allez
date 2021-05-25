91aed67221897cf62491f1a248a012e55c4ee5e0
Jonathan Lee <jon.lee.jing.yu@gmail.com>
2021-05-25 20:06:05 +0800

    add: server env variables

--- server/routes/api/Users.js
+++ server/routes/api/Users.js
@@ -50,15 +50,15 @@ const nodemailer = require("nodemailer");
 
 /** SMTP configuration. TODO: Store real credentials in env */
 const transporter = nodemailer.createTransport({
-    host: "smtp.ethereal.email",
-    port: 587,
-    secure: false,
-    auth: {
-        // spoof credentials from ethereal
-        // can login from here to access the emails https://ethereal.email/
-        user: "malinda66@ethereal.email",
-        pass: "mg36XzUWRXTvGN1GGG",
-    },
+  host: "smtp.ethereal.email",
+  port: 587,
+  secure: false,
+  auth: {
+    // spoof credentials from ethereal
+    // can login from here to access the emails https://ethereal.email/
+    user: "malinda66@ethereal.email",
+    pass: "mg36XzUWRXTvGN1GGG",
+  },
 });
 
 // Validate inputs
@@ -76,53 +76,52 @@ const User = require("../../models/User");
  * @param {object} res Server response
  */
 async function handleRegister(req, res) {
-    User.findOne({ email: req.body.email }).then((user) => {
-        if (user) {
-            return res.status(400).json({ email: "Email already exists" });
-        } else {
-            // TODO: Insert validator? maybe
-            const newUser = new User({
-                username: req.body.username,
-                email: req.body.email,
-                password: req.body.password,
-                activated: false,
-            });
+  User.findOne({ email: req.body.email }).then((user) => {
+    if (user) {
+      return res.status(400).json({ email: "Email already exists" });
+    } else {
+      // TODO: Insert validator? maybe
+      const newUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
+        activated: false,
+      });
 
-            bcrypt.genSalt().then((salt) => {
-                bcrypt.hash(newUser.password, salt, (err, hash) => {
-                    if (err) {
-                        return res.status(400).json(err);
-                    }
-                    newUser.password = hash;
-                    let id;
-                    newUser
-                        .save()
-                        .then(async (user) => {
-                            const token = jwt.sign(
-                                { id: user.id },
-                                process.env.JWT_SECRET
-                            );
-                            // sends token to the registered email for confirmation;
-                            await transporter.sendMail(
-                                {
-                                    from: '"Allez" <reset@allez.com>',
-                                    to: `${newUser.email}`,
-                                    subject: "Please confirm your account",
-                                    text: `Click here to confirm your account.`,
-                                    html: `Click <a href = "http://localhost:3000/confirm/token=${token}">here</a> to confirm your account.`,
-                                },
-                                (err, info) => {
-                                    res.status(400).json({ message: err });
-                                    res.status(200).json({ message: info });
-                                }
-                            );
-                            res.json(user);
-                        })
-                        .catch((err) => res.status(500).json(err));
-                });
-            });
-        }
-    });
+      bcrypt.genSalt().then((salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) {
+            return res.status(400).json(err);
+          }
+          newUser.password = hash;
+          let id;
+          newUser
+            .save()
+            .then(async (user) => {
+              const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET);
+              // sends token to the registered email for confirmation;
+              await transporter.sendMail(
+                {
+                  from: '"Allez" <reset@allez.com>',
+                  to: `${newUser.email}`,
+                  subject: "Please confirm your account",
+                  text: `Click here to confirm your account.`,
+                  html: `Click <a href = "${process.env.DOMAIN}/confirm/token=${token}">here</a> to confirm your account.`,
+                },
+                (err, info) => {
+                  if (err) {
+                    return res.status(400).json({ message: err });
+                  }
+                  return res.status(200).json({ message: info });
+                }
+              );
+              return res.json(user);
+            })
+            .catch((err) => res.status(500).json(err));
+        });
+      });
+    }
+  });
 }
 
 /**
@@ -138,42 +137,40 @@ async function handleRegister(req, res) {
  */
 
 async function handleLogin(req, res) {
-    try {
-        const { email, password } = req.body;
-        if (!email || !password) {
-            return res
-                .status(400)
-                .json({ message: "Not all fields have been entered." });
-        } else {
-            const user = await User.findOne({ email: email });
-            if (!user) {
-                return res.status(401).json({ message: " Invalid user" });
-            } else if (!user.activated) {
-                return res
-                    .status(403)
-                    .json({ message: "Account not activated" });
-            }
-            // use bcrypt to compare crypto hash from db and given password
-            const checkPassword = await bcrypt.compare(password, user.password);
-            if (!checkPassword) {
-                return res.status(401).json({ message: "Invalid credentials" });
-            }
-            // use jwt to sign our user ID to be decoded later, using the key from the .env file
-            const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ message: "Not all fields have been entered." });
+    } else {
+      const user = await User.findOne({ email: email });
+      if (!user) {
+        return res.status(401).json({ message: " Invalid user" });
+      } else if (!user.activated) {
+        return res.status(403).json({ message: "Account not activated" });
+      }
+      // use bcrypt to compare crypto hash from db and given password
+      const checkPassword = await bcrypt.compare(password, user.password);
+      if (!checkPassword) {
+        return res.status(401).json({ message: "Invalid credentials" });
+      }
+      // use jwt to sign our user ID to be decoded later, using the key from the .env file
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
 
-            // returns status 200 with the token and the user information
-            return res.status(200).json({
-                token,
-                user: {
-                    id: user._id,
-                    username: user.username,
-                    email: user.email,
-                },
-            });
-        }
-    } catch (err) {
-        return res.status(500).json({ error: err.message });
+      // returns status 200 with the token and the user information
+      return res.status(200).json({
+        token,
+        user: {
+          id: user._id,
+          username: user.username,
+          email: user.email,
+        },
+      });
     }
+  } catch (err) {
+    return res.status(500).json({ error: err.message });
+  }
 }
 
 /**
@@ -189,30 +186,30 @@ async function handleLogin(req, res) {
  */
 
 async function handleVerify(req, res) {
-    const token = req.header("token");
-    if (!token) {
-        return res.status(400).json({ message: "No token" });
+  const token = req.header("token");
+  if (!token) {
+    return res.status(400).json({ message: "No token" });
+  } else {
+    const verified = jwt.verify(token, process.env.JWT_SECRET);
+    if (!verified) {
+      return res.status(401).json({ message: "Failed" });
     } else {
-        const verified = jwt.verify(token, process.env.JWT_SECRET);
-        if (!verified) {
-            return res.status(401).json({ message: "Failed" });
-        } else {
-            // decoding the token returns us our objectId that we encoded
-            const userId = jwt.decode(token, process.env.JWT_SECRET).id;
-            console.log(userId);
-            let user;
-            try {
-                user = await User.findOne(new ObjectId(userId));
-                return res.json({
-                    username: user.username,
-                    email: user.email,
-                });
-            } catch (err) {
-                // return error if we somehow encounter an error finding the user
-                return res.status(500).json(err);
-            }
-        }
+      // decoding the token returns us our objectId that we encoded
+      const userId = jwt.decode(token, process.env.JWT_SECRET).id;
+      console.log(userId);
+      let user;
+      try {
+        user = await User.findOne(new ObjectId(userId));
+        return res.json({
+          username: user.username,
+          email: user.email,
+        });
+      } catch (err) {
+        // return error if we somehow encounter an error finding the user
+        return res.status(500).json(err);
+      }
     }
+  }
 }
 
 /**
@@ -224,42 +221,42 @@ async function handleVerify(req, res) {
  */
 
 async function handleResetRequest(req, res) {
-    const email = req.body.email;
-    // Ensure the email is specified
-    if (!email) {
-        return res.status(400).json({ message: "Email not specified" });
-    } else {
-        try {
-            // ensure we can find the user
-            const user = await User.findOne({ email: email });
-            if (!user) {
-                return res.status(401).json({ message: "Invalid user" });
-            } else {
-                const secret = user._id + "-" + process.env.JWT_TOKEN;
-                // currently set the token to expire in 10 minutes
-                const token = jwt.sign({ id: user._id }, secret, {
-                    expiresIn: 600,
-                });
-                // auto email sender to send recovery link to reset password.
-                await transporter.sendMail(
-                    {
-                        from: '"Allez" <reset@allez.com>',
-                        to: `${req.body.email}`,
-                        subject: "You requested for a password reset",
-                        text: `${token}`,
-                        html: `<b>${token}</b>`,
-                    },
-                    (err, info) => {
-                        console.log(err);
-                        console.log(info);
-                    }
-                );
-                return res.status(200).send(token);
-            }
-        } catch (err) {
+  const email = req.body.email;
+  // Ensure the email is specified
+  if (!email) {
+    return res.status(400).json({ message: "Email not specified" });
+  } else {
+    try {
+      // ensure we can find the user
+      const user = await User.findOne({ email: email });
+      if (!user) {
+        return res.status(401).json({ message: "Invalid user" });
+      } else {
+        const secret = user._id + "-" + process.env.JWT_TOKEN;
+        // currently set the token to expire in 10 minutes
+        const token = jwt.sign({ id: user._id }, secret, {
+          expiresIn: 600,
+        });
+        // auto email sender to send recovery link to reset password.
+        await transporter.sendMail(
+          {
+            from: '"Allez" <reset@allez.com>',
+            to: `${req.body.email}`,
+            subject: "You requested for a password reset",
+            text: `Click here to reset your account.`,
+            html: `Click <a href = "${process.env.DOMAIN}/recover/token=${token}">here</a> to confirm your account.`,
+          },
+          (err, info) => {
             console.log(err);
-        }
+            console.log(info);
+          }
+        );
+        return res.status(200).send(token);
+      }
+    } catch (err) {
+      console.log(err);
     }
+  }
 }
 
 /**
@@ -273,64 +270,59 @@ async function handleResetRequest(req, res) {
  */
 
 async function handleReset(req, res) {
-    const token = req.body.token;
-    if (!token) {
-        return res.status(400).json({ message: "No token" });
-    } else {
-        try {
-            const id = req.body.id;
-            const user = await User.findOne(new ObjectId(id));
-            if (!user) {
-                return res.status(400).json({ message: "Invalid user ID" });
-            } else {
-                // verify to ensure token has not expired
-                jwt.verify(
-                    req.body.token,
-                    user.id + "-" + process.env.JWT_SECRET
-                );
-                // decode to ensure the token matches the userId
-                decoded = jwt.decode(
-                    req.body.token,
-                    user.id + "-" + process.env.JWT_SECRET
-                ).id;
-                if (decoded == user.id) {
-                    let password;
-                    // generate password hash for the new password
-                    bcrypt.genSalt().then((salt) =>
-                        bcrypt.hash(req.body.password, salt, (err, hash) => {
-                            if (err) {
-                                return res
-                                    .status(500)
-                                    .json({ message: "Password error" });
-                            } else {
-                                password = hash;
-                            }
-                        })
-                    );
-                    // update the password field for this user
-                    await User.findByIdAndUpdate(
-                        new ObjectId(req.body.id),
-                        {
-                            password: password,
-                        },
-                        { useFindAndModify: false }
-                    );
-                    return res
-                        .status(200)
-                        .json({ message: "Password successfully updated" });
-                } else {
-                    return res.status(401).json({ message: "Unauthorized" });
-                }
-            }
-        } catch (err) {
-            // catch the error thrown if the token has expired.
-            if (err instanceof jwt.TokenExpiredError) {
-                return res.status(401).json({ message: "Token has expired" });
-            }
-            // console log for now. TODO: implement error handling
-            console.log(err);
+  const token = req.body.token;
+  if (!token) {
+    return res.status(400).json({ message: "No token" });
+  } else {
+    try {
+      const id = req.body.id;
+      const user = await User.findOne(new ObjectId(id));
+      if (!user) {
+        return res.status(400).json({ message: "Invalid user ID" });
+      } else {
+        // verify to ensure token has not expired
+        jwt.verify(req.body.token, user.id + "-" + process.env.JWT_SECRET);
+        // decode to ensure the token matches the userId
+        decoded = jwt.decode(
+          req.body.token,
+          user.id + "-" + process.env.JWT_SECRET
+        ).id;
+        if (decoded == user.id) {
+          let password;
+          // generate password hash for the new password
+          bcrypt.genSalt().then((salt) =>
+            bcrypt.hash(req.body.password, salt, (err, hash) => {
+              if (err) {
+                return res.status(500).json({ message: "Password error" });
+              } else {
+                password = hash;
+              }
+            })
+          );
+          // update the password field for this user
+          await User.findByIdAndUpdate(
+            new ObjectId(req.body.id),
+            {
+              password: password,
+            },
+            { useFindAndModify: false }
+          );
+          return res
+            .status(200)
+            .json({ message: "Password successfully updated" });
+        } else {
+          return res.status(401).json({ message: "Unauthorized" });
         }
+      }
+    } catch (err) {
+      // catch the error thrown if the token has expired.
+      if (err instanceof jwt.TokenExpiredError) {
+        return res.status(401).json({ message: "Token has expired" });
+      }
+      // console log for now. TODO: implement error handling
+      console.log(err);
     }
+  }
 }
 
 /**
@@ -342,37 +334,32 @@ async function handleReset(req, res) {
  */
 
 async function handleConfirm(req, res) {
-    if (!req.headers) {
-        return res.status(400).json({ message: "Missing header" });
+  if (!req.headers) {
+    return res.status(400).json({ message: "Missing header" });
+  } else {
+    if (!req.headers.token) {
+      return res.status(400).json({ message: "Missing token" });
     } else {
-        if (!req.headers.token) {
-            return res.status(400).json({ message: "Missing token" });
-        } else {
-            try {
-                const id = jwt.decode(
-                    req.headers.token,
-                    process.env.JWT_SECRET
-                ).id;
-                const user = await User.findOne(new ObjectId(id));
-                if (user.activated) {
-                    return res
-                        .status(403)
-                        .json({ message: "Account already activated" });
-                }
-                console.log(id);
-                await User.findByIdAndUpdate(
-                    new ObjectId(id),
-                    { activated: true },
-                    { useFindAndModify: false }
-                );
-                return res
-                    .status(200)
-                    .json({ message: "Account successfully activated" });
-            } catch (err) {
-                return res.status(400).json({ message: err });
-            }
+      try {
+        const id = jwt.decode(req.headers.token, process.env.JWT_SECRET).id;
+        const user = await User.findOne(new ObjectId(id));
+        if (user.activated) {
+          return res.status(403).json({ message: "Account already activated" });
         }
+        console.log(id);
+        await User.findByIdAndUpdate(
+          new ObjectId(id),
+          { activated: true },
+          { useFindAndModify: false }
+        );
+        return res
+          .status(200)
+          .json({ message: "Account successfully activated" });
+      } catch (err) {
+        return res.status(400).json({ message: err });
+      }
     }
+  }
 }
 
 /** Provides the route for the API at ./register using the handleRegister function. */
