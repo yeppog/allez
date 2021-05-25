65795ba00604e9c0db019633d5c40617fbb22140
Jonathan Lee <jon.lee.jing.yu@gmail.com>
2021-05-25 19:18:45 +0800

    feat: mui for register component and refactoring

--- client/src/components/Auth/RegisterComponent/RegisterComponent.tsx
+++ client/src/components/Auth/RegisterComponent/RegisterComponent.tsx
@@ -1,59 +1,185 @@
-import React from 'react';
 import './RegisterComponent.scss';
-import { useState } from 'react'
-import axios from 'axios'
-import { RegisterCredentials } from '../../../interface/Credentials'
+
+import {
+  Button,
+  Card,
+  FormControl,
+  Grid,
+  IconButton,
+  Input,
+  InputAdornment,
+  InputLabel,
+} from '@material-ui/core';
+import { Visibility, VisibilityOff } from '@material-ui/icons';
+
+import React from 'react';
+import { RegisterCredentials } from '../../../interface/Credentials';
+import axios from 'axios';
+import { useState } from 'react';
 
 const RegisterComponent: React.FC = () => {
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('')
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [credentials, setCredentials] = useState({
+    name: '',
+    username: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
 
   // Redudant code just to display in frontend instead of console.logging , can remove later
   const [message, setMessage] = useState('');
 
+  interface State {
+    name: string;
+    username: string;
+    email: string;
+    password: string;
+    confirmPassword: string;
+  }
+
+  const handleChange =
+    (prop: keyof State) => (event: React.ChangeEvent<HTMLInputElement>) => {
+      setCredentials({ ...credentials, [prop]: event.target.value });
+    };
+
   /**
    * Passes the credentials the user has entered to the redux handler to log the user in.
-   * 
+   *
    * @param e Event that is passed in when the form is submitted
    */
-
   const onSubmit = (e: any): void => {
     // preventDefault prevents the page from refreshing when the form is submitted
     e.preventDefault();
     // simple validator for now
     // TODO: Better validation for email and password
-    if (username && email && password && (password === confirmPassword)) {
+    if (validator(credentials)) {
+      const username = credentials.username;
+      const email = credentials.email;
+      const password = credentials.password;
       const credential = { username, email, password } as RegisterCredentials;
       registerLogin(credential);
       console.log(credential);
-      setMessage("Valid");
+      setMessage('Valid');
     } else {
-      console.log("Form is invalid or passwords do not match");
+      console.log('Form is invalid or passwords do not match');
     }
-  }
+  };
 
   const registerLogin = (credentials: RegisterCredentials): void => {
     try {
       console.log(credentials);
-      const url = "http://localhost:3001/api/users/register";
-      axios.post(url, credentials, HTTPOptions)
-        .then(data => console.log(data))
-        .catch(err => console.log(err));
+      const url = 'http://localhost:3001/api/users/register';
+      axios
+        .post(url, credentials, HTTPOptions)
+        .then((data) => console.log(data))
+        .catch((err) => console.log(err));
     } catch (err) {
       console.log(err);
     }
-  }
+  };
+
+  const validator = (form: State): boolean => {
+    if (form.name && form.email && form.username) {
+      if (form.password === form.confirmPassword) {
+        const email = form.email;
+        const re =
+          /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(String(email).toLowerCase());
+      }
+    }
+    return false;
+  };
 
   const HTTPOptions = {
-    headers: { 'Content-Type': 'application/json' }
-  }
-
+    headers: { 'Content-Type': 'application/json' },
+  };
 
   return (
     <div className="RegisterComponent" data-testid="RegisterComponent">
-      <form action="" className="registerForm" onSubmit={e => onSubmit(e)}>
+      <Card className="registerForm" variant="outlined">
+        <h2>Register</h2>
+        <form onSubmit={(e) => onSubmit(e)}>
+          <Grid>
+            <Grid item>
+              <FormControl fullWidth variant="filled">
+                <InputLabel>Full Name</InputLabel>
+                <Input
+                  fullWidth
+                  className="name"
+                  type="text"
+                  value={credentials.name}
+                  onChange={handleChange('name')}
+                />
+              </FormControl>
+            </Grid>
+            <Grid item>
+              <FormControl fullWidth variant="filled">
+                <InputLabel>Username</InputLabel>
+                <Input
+                  fullWidth
+                  className="username"
+                  type="text"
+                  value={credentials.username}
+                  onChange={handleChange('username')}
+                />
+              </FormControl>
+            </Grid>
+            <Grid item>
+              <FormControl fullWidth variant="filled">
+                <InputLabel>Email</InputLabel>
+                <Input
+                  fullWidth
+                  className="email"
+                  type="text"
+                  value={credentials.email}
+                  onChange={handleChange('email')}
+                />
+              </FormControl>
+            </Grid>
+            <Grid item>
+              <FormControl fullWidth variant="filled">
+                <InputLabel>Password</InputLabel>
+                <Input
+                  fullWidth
+                  className="password"
+                  type="password"
+                  value={credentials.password}
+                  onChange={handleChange('password')}
+                />
+              </FormControl>
+            </Grid>
+            <Grid item>
+              <FormControl fullWidth variant="filled">
+                <InputLabel>Confirm Password</InputLabel>
+                <Input
+                  fullWidth
+                  className="cpassword"
+                  type="password"
+                  value={credentials.confirmPassword}
+                  onChange={handleChange('confirmPassword')}
+                />
+              </FormControl>
+            </Grid>
+            <Grid item className="registerButton">
+              <Button
+                fullWidth
+                className="loginButton"
+                variant="text"
+                color="default"
+                type="submit"
+              >
+                Register
+              </Button>
+            </Grid>
+          </Grid>
+        </form>
+      </Card>
+
+      {/* <form action="" className="registerForm" onSubmit={e => onSubmit(e)}>
         <h2>Registration</h2>
         <input
           type="text"
@@ -96,9 +222,9 @@ const RegisterComponent: React.FC = () => {
         }
 
 
-      </form>
+      </form> */}
     </div>
-  )
+  );
 };
 
 export default RegisterComponent;
