611f4e584fa55d3973a5ac291044574b4bbbf15c
Jonathan Lee <jon.lee.jing.yu@gmail.com>
2021-05-25 18:43:10 +0800

    feat: mui for login component and refactoring

--- client/src/components/Auth/LoginComponent/LoginComponent.tsx
+++ client/src/components/Auth/LoginComponent/LoginComponent.tsx
@@ -1,33 +1,62 @@
 import './LoginComponent.scss';
 
+import {
+  Button,
+  Card,
+  FormControl,
+  Grid,
+  IconButton,
+  Input,
+  InputAdornment,
+  InputLabel,
+} from '@material-ui/core';
+import { Visibility, VisibilityOff } from '@material-ui/icons';
 import { checkLoggedInUser, loginUser } from '../../Redux/userSlice';
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react';
 
 import { LoginCredentials } from '../../../interface/Credentials';
 import React from 'react';
-import { useDispatch } from 'react-redux'
+import { useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 
+interface State {
+  email: string;
+  password: string;
+  showPassword: boolean;
+}
+
 const LoginComponent: React.FC = () => {
-
-  const [username, setUsername] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
-  // Redudant code just to display in frontend instead of console.logging , can remove later
-  const [message, setMessage] = useState('');
+  const [credentials, setCredentials] = useState<State>({
+    email: '',
+    password: '',
+    showPassword: false,
+  });
 
   const dispatch = useDispatch();
   const history = useHistory();
 
+  /**
+   * Helper to set state for hooks
+   */
+  const handleChange =
+    (prop: keyof State) => (event: React.ChangeEvent<HTMLInputElement>) => {
+      setCredentials({ ...credentials, [prop]: event.target.value });
+    };
+
+  /**
+   * Helper to toggle the input type of the pasword field
+   */
+  const handleClickShowPassword = () => {
+    setCredentials({ ...credentials, showPassword: !credentials.showPassword });
+  };
+
   useEffect(() => {
     dispatch(checkLoggedInUser());
-  }, [])
-
+  }, []);
 
   /**
    * Passes the credentials the user has entered to the redux handler to log the user in.
-   * 
+   *
    * @param e Event that is passed in when the form is submitted
    */
 
@@ -36,71 +65,91 @@ const LoginComponent: React.FC = () => {
     e.preventDefault();
     // simple validator for now
     // TODO: Better validation for email and password
-    if (username && email && password) {
-      const credential = { username, email, password } as LoginCredentials;
+    if (credentials.email && credentials.password) {
+      const email = credentials.email;
+      const password = credentials.password;
+      const credential = { email, password } as LoginCredentials;
       dispatch(loginUser(credential));
-      console.log(credential);
-      setMessage("Valid");
+      console.log('Request dispatched to server');
     } else {
-      console.log("Form is invalid");
+      console.log('Form is invalid');
     }
-  }
-
-  // const handleLogin = (credentials: LoginCredentials): void => {
-  //   try {
-  //     const url = "http://localhost:3001/api/users/login";
-  //     axios.post(url, credentials, HTTPOptions)
-  //       .then(data => console.log(data))
-  //       .catch(err => console.log(err));
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
-  // }
-
-
+  };
 
   return (
     <div className="LoginComponent" data-testid="LoginComponent">
-      <form action="" className="loginForm" onSubmit={e => onSubmit(e)}>
+      <Card className="loginForm" variant="outlined">
         <h2>Login</h2>
-        <input
-          type="text"
-          className="username"
-          placeholder="username"
-          onChange={e => setUsername(e.target.value)}
-        />
-
-        <input
-          type="text"
-          className="email"
-          placeholder="email"
-          onChange={e => setEmail(e.target.value)}
-        />
-        <input
-          type="text"
-          className="password"
-          placeholder="password"
-          onChange={e => setPassword(e.target.value)}
-        />
-
-        <button className="submit" type="submit">Submit</button>
-        <button className="register" type="button" onClick={() => history.push('/register')}>Register for an account</button>
-
-        <p>username: {username}</p>
-        <p>email: {email}</p>
-        <p>password: {password}</p>
-
-        {message ?
-          <p>
-            {message}
-          </p>
-          : <div></div>
-        }
-
-
-      </form>
+        <form onSubmit={(e) => onSubmit(e)}>
+          <Grid>
+            <Grid item>
+              <FormControl fullWidth variant="filled">
+                <InputLabel>Username / Email</InputLabel>
+                <Input
+                  fullWidth
+                  className="username"
+                  type="text"
+                  id="email"
+                  value={credentials.email}
+                  onChange={handleChange('email')}
+                ></Input>
+              </FormControl>
+            </Grid>
+            <Grid item>
+              <FormControl fullWidth variant="filled">
+                <InputLabel>Password</InputLabel>
+                <Input
+                  fullWidth
+                  className="password"
+                  type={credentials.showPassword ? 'text' : 'password'}
+                  id="standard-adornment-password"
+                  value={credentials.password}
+                  onChange={handleChange('password')}
+                  endAdornment={
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={handleClickShowPassword}
+                      >
+                        {credentials.showPassword ? (
+                          <Visibility />
+                        ) : (
+                          <VisibilityOff />
+                        )}
+                      </IconButton>
+                    </InputAdornment>
+                  }
+                />
+              </FormControl>
+            </Grid>
+            <Grid item className="loginButton">
+              <Button
+                fullWidth
+                className="loginButton"
+                variant="text"
+                color="default"
+                type="submit"
+              >
+                Login
+              </Button>
+            </Grid>
+            <Grid item className="registerButton">
+              <Button
+                fullWidth
+                className="registerButton"
+                variant="text"
+                color="default"
+                type="button"
+                onClick={() => history.push('/register')}
+              >
+                Register
+              </Button>
+            </Grid>
+          </Grid>
+        </form>
+      </Card>
     </div>
-  )
+  );
 };
 
 export default LoginComponent;
