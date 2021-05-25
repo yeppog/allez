5217c107491e8d0b061fb186c544395c1fb512e9
Jonathan Lee <jon.lee.jing.yu@gmail.com>
2021-05-26 00:41:37 +0800

    feat: mobile view for register, add alert modal

--- client/src/components/Auth/RegisterComponent/RegisterComponent.tsx
+++ client/src/components/Auth/RegisterComponent/RegisterComponent.tsx
@@ -3,6 +3,7 @@ import './RegisterComponent.scss';
 import {
   Button,
   Card,
+  Fade,
   FormControl,
   Grid,
   IconButton,
@@ -12,42 +13,42 @@ import {
 } from '@material-ui/core';
 import { Visibility, VisibilityOff } from '@material-ui/icons';
 
+import { Alert } from '@material-ui/lab';
 import React from 'react';
 import { RegisterCredentials } from '../../../interface/Credentials';
 import axios from 'axios';
+import { useHistory } from 'react-router';
 import { useState } from 'react';
 
+interface State {
+  name: string;
+  username: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+  postSuccess: boolean;
+  message: string;
+}
+
 const RegisterComponent: React.FC = () => {
-  const [username, setUsername] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
-  const [credentials, setCredentials] = useState({
+  const history = useHistory();
+  const [state, setState] = useState({
     name: '',
     username: '',
     email: '',
     password: '',
     confirmPassword: '',
+    postSuccess: false,
+    message: '',
   });
-
-  // Redudant code just to display in frontend instead of console.logging , can remove later
-  const [message, setMessage] = useState('');
-
-  interface State {
-    name: string;
-    username: string;
-    email: string;
-    password: string;
-    confirmPassword: string;
-  }
 
   const handleChange =
     (prop: keyof State) => (event: React.ChangeEvent<HTMLInputElement>) => {
-      setCredentials({ ...credentials, [prop]: event.target.value });
+      setState({ ...state, [prop]: event.target.value });
     };
 
   /**
-   * Passes the credentials the user has entered to the redux handler to log the user in.
+   * Passes the state the user has entered to the redux handler to log the user in.
    *
    * @param e Event that is passed in when the form is submitted
    */
@@ -56,14 +57,12 @@ const RegisterComponent: React.FC = () => {
     e.preventDefault();
     // simple validator for now
     // TODO: Better validation for email and password
-    if (validator(credentials)) {
-      const username = credentials.username;
-      const email = credentials.email;
-      const password = credentials.password;
+    if (validator(state)) {
+      const username = state.username;
+      const email = state.email;
+      const password = state.password;
       const credential = { username, email, password } as RegisterCredentials;
       registerLogin(credential);
-      console.log(credential);
-      setMessage('Valid');
     } else {
       console.log('Form is invalid or passwords do not match');
     }
@@ -71,14 +70,18 @@ const RegisterComponent: React.FC = () => {
 
   const registerLogin = (credentials: RegisterCredentials): void => {
     try {
-      console.log(credentials);
-      const url = 'http://localhost:3001/api/users/register';
+      const url = '/api/users/register';
       axios
         .post(url, credentials, HTTPOptions)
-        .then((data) => console.log(data))
-        .catch((err) => console.log(err));
+        .then((data) => {
+          setState({ ...state, postSuccess: true });
+          console.log(state.postSuccess);
+        })
+        .catch((err) => {
+          setState({ ...state, message: err.message });
+        });
     } catch (err) {
-      console.log(err);
+      setState({ ...state, message: err.message });
     }
   };
 
@@ -86,6 +89,7 @@ const RegisterComponent: React.FC = () => {
     if (form.name && form.email && form.username) {
       if (form.password === form.confirmPassword) {
         const email = form.email;
+        // regex email checker
         const re =
           /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         return re.test(String(email).toLowerCase());
@@ -100,84 +104,122 @@ const RegisterComponent: React.FC = () => {
 
   return (
     <div className="RegisterComponent" data-testid="RegisterComponent">
-      <Card className="registerForm" variant="outlined">
-        <h2>Register</h2>
-        <form onSubmit={(e) => onSubmit(e)}>
-          <Grid>
-            <Grid item>
-              <FormControl fullWidth variant="filled">
-                <InputLabel>Full Name</InputLabel>
-                <Input
-                  fullWidth
-                  className="name"
-                  type="text"
-                  value={credentials.name}
-                  onChange={handleChange('name')}
-                />
-              </FormControl>
-            </Grid>
-            <Grid item>
-              <FormControl fullWidth variant="filled">
-                <InputLabel>Username</InputLabel>
-                <Input
-                  fullWidth
-                  className="username"
-                  type="text"
-                  value={credentials.username}
-                  onChange={handleChange('username')}
-                />
-              </FormControl>
-            </Grid>
-            <Grid item>
-              <FormControl fullWidth variant="filled">
-                <InputLabel>Email</InputLabel>
-                <Input
-                  fullWidth
-                  className="email"
-                  type="text"
-                  value={credentials.email}
-                  onChange={handleChange('email')}
-                />
-              </FormControl>
-            </Grid>
-            <Grid item>
-              <FormControl fullWidth variant="filled">
-                <InputLabel>Password</InputLabel>
-                <Input
-                  fullWidth
-                  className="password"
-                  type="password"
-                  value={credentials.password}
-                  onChange={handleChange('password')}
-                />
-              </FormControl>
-            </Grid>
-            <Grid item>
-              <FormControl fullWidth variant="filled">
-                <InputLabel>Confirm Password</InputLabel>
-                <Input
-                  fullWidth
-                  className="cpassword"
-                  type="password"
-                  value={credentials.confirmPassword}
-                  onChange={handleChange('confirmPassword')}
-                />
-              </FormControl>
-            </Grid>
-            <Grid item className="registerButton">
-              <Button
-                fullWidth
-                className="loginButton"
-                variant="text"
-                color="default"
-                type="submit"
-              >
-                Register
-              </Button>
-            </Grid>
-          </Grid>
-        </form>
-      </Card>
+      <Grid container>
+        <Grid item xs={12}>
+          <Card className="registerForm" variant="outlined">
+            {state.postSuccess ? (
+              <div>
+                <Fade in={state.postSuccess}>
+                  <Grid item sm={12}>
+                    <h2>Thank you for registering</h2>
+                    <p>
+                      Please check your email account for a verification mail we
+                      just sent to activate your account
+                    </p>
+                    <Button onClick={(e) => history.push('/login')}>
+                      Login Page
+                    </Button>
+                  </Grid>
+                </Fade>
+              </div>
+            ) : (
+              <div>
+                <h2>Register</h2>
+                <form onSubmit={(e) => onSubmit(e)}>
+                  <Grid item sm={12}>
+                    <FormControl fullWidth variant="filled">
+                      <InputLabel>Full Name</InputLabel>
+                      <Input
+                        fullWidth
+                        className="name"
+                        type="text"
+                        value={state.name}
+                        onChange={handleChange('name')}
+                      />
+                    </FormControl>
+                  </Grid>
+                  <Grid item>
+                    <FormControl fullWidth variant="filled">
+                      <InputLabel>Username</InputLabel>
+                      <Input
+                        fullWidth
+                        className="username"
+                        type="text"
+                        value={state.username}
+                        onChange={handleChange('username')}
+                        required
+                      />
+                    </FormControl>
+                  </Grid>
+                  <Grid item>
+                    <FormControl fullWidth variant="filled">
+                      <InputLabel>Email</InputLabel>
+                      <Input
+                        fullWidth
+                        className="email"
+                        type="email"
+                        value={state.email}
+                        onChange={handleChange('email')}
+                        required
+                      />
+                    </FormControl>
+                  </Grid>
+                  <Grid item>
+                    <FormControl fullWidth variant="filled">
+                      <InputLabel>Password</InputLabel>
+                      <Input
+                        fullWidth
+                        className="password"
+                        type="password"
+                        value={state.password}
+                        onChange={handleChange('password')}
+                        required
+                      />
+                    </FormControl>
+                  </Grid>
+                  <Grid item>
+                    <FormControl fullWidth variant="filled">
+                      <InputLabel>Confirm Password</InputLabel>
+                      <Input
+                        fullWidth
+                        className="cpassword"
+                        type="password"
+                        value={state.confirmPassword}
+                        onChange={handleChange('confirmPassword')}
+                        required
+                      />
+                    </FormControl>
+                  </Grid>
+                  {state.message && (
+                    <Fade in={state.message ? true : false}>
+                      <div className="errorAlert">
+                        <Alert
+                          severity="error"
+                          onClose={() => setState({ ...state, message: '' })}
+                        >
+                          {state.message}
+                        </Alert>
+                      </div>
+                    </Fade>
+                  )}
+
+                  <Grid item className="registerButton">
+                    <Button
+                      fullWidth
+                      className="registerButton"
+                      variant="text"
+                      color="default"
+                      type="submit"
+                    >
+                      Register
+                    </Button>
+                  </Grid>
+                </form>
+              </div>
+            )}
+          </Card>
+        </Grid>
+      </Grid>
 
       {/* <form action="" className="registerForm" onSubmit={e => onSubmit(e)}>
         <h2>Registration</h2>
