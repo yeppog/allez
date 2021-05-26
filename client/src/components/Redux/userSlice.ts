bc2245808b9f8d2dda100696c39d9f5505a2b62c
Jonathan Lee <jon.lee.jing.yu@gmail.com>
2021-05-26 18:08:12 +0800

    fix: ensure no jwt throws error rejects promise for store

--- client/src/components/Redux/userSlice.ts
+++ client/src/components/Redux/userSlice.ts
@@ -1,88 +1,80 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
-import { LoginCredentials } from "../../interface/Credentials";
-import axios from "axios";
-
+import { LoginCredentials } from '../../interface/Credentials';
+import axios from 'axios';
 
 // sets the default axios baseURL to the environmental variable
 axios.defaults.baseURL = process.env.REACT_APP_API_URI;
 
 interface UsersState {
   user: string[];
-  status: "idle" | "pending" | "succeeded" | "failed";
+  status: 'idle' | 'pending' | 'succeeded' | 'failed';
   error: string | null | undefined;
 }
 
 const initialState = {
   user: [],
-  status: "idle",
+  status: 'idle',
   error: null,
 } as UsersState;
 
 export const loginUser = createAsyncThunk(
-  "user/loginUser",
+  'user/loginUser',
   async (body: LoginCredentials) => {
-    const res = await axios.post(
-      "/api/users/login",
-      body,
-      HTTPOptions
-    );
+    const res = await axios.post('/api/users/login', body, HTTPOptions);
     return res.data;
   }
 );
 
 export const checkLoggedInUser = createAsyncThunk(
-  "user/checkLoggedInUser",
+  'user/checkLoggedInUser',
   async () => {
-    const token = localStorage.getItem("token");
+    const token = localStorage.getItem('token');
     if (!token) {
-      return { error: "User is not logged in." };
+      throw Error('No Token');
     } else {
-      console.log(token);
-      const res = await axios.get(
-        "/api/users/verify",
-        { headers: { ...HTTPOptions.headers, token: token } }
-      );
-
+      const res = await axios.get('/api/users/verify', {
+        headers: { ...HTTPOptions.headers, token: token },
+      });
       return res.data;
     }
   }
 );
 
 const userSlice = createSlice({
-  name: "user",
+  name: 'user',
   initialState,
   reducers: {},
   extraReducers: (builder) => {
     builder.addCase(loginUser.pending, (state, action) => {
-      state.status = "pending";
+      state.status = 'pending';
     });
     builder.addCase(loginUser.fulfilled, (state, action) => {
-      state.status = "succeeded";
+      state.status = 'succeeded';
       state.user = state.user.concat(action.payload);
-      localStorage.setItem("token", action.payload.token);
-      console.log(localStorage.getItem("token"));
+      localStorage.setItem('token', action.payload.token);
+      console.log(localStorage.getItem('token'));
     });
     builder.addCase(loginUser.rejected, (state, action) => {
-      state.status = "failed";
+      state.status = 'failed';
       state.error = action.error.message;
     });
     builder.addCase(checkLoggedInUser.pending, (state, action) => {
-      state.status = "pending";
+      state.status = 'pending';
     });
     builder.addCase(checkLoggedInUser.fulfilled, (state, action) => {
-      state.status = "succeeded";
+      state.status = 'succeeded';
       state.user = state.user.concat(action.payload);
     });
     builder.addCase(checkLoggedInUser.rejected, (state, action) => {
-      state.status = "failed";
+      state.status = 'failed';
       state.error = action.error.message;
     });
   },
 });
 
 const HTTPOptions = {
-  headers: { "Content-Type": "application/json" },
+  headers: { 'Content-Type': 'application/json' },
 };
 
 export default userSlice.reducer;
