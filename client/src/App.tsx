c05c0a010fc0e858dabe69ec8a8f9ea1a9a66b84
Jonathan Lee <jon.lee.jing.yu@gmail.com>
2021-05-27 02:36:12 +0800

    feat: hide bot nav on desktop

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -84,7 +84,7 @@ function App() {
         />
         <Route path="*" component={NotFoundComponent} />
       </Switch>
-      <footer>
+      <footer className="footer">
         <BotNavComponent />
       </footer>
     </div>
