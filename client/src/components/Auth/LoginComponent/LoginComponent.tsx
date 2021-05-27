d8c358e167f65b799c15ef8bd5a5e299b53e66bd
Jonathan Lee <jon.lee.jing.yu@gmail.com>
2021-05-27 20:49:02 +0800

    feat: disable login access when logged in

--- client/src/components/Auth/LoginComponent/LoginComponent.tsx
+++ client/src/components/Auth/LoginComponent/LoginComponent.tsx
@@ -26,13 +26,15 @@ interface State {
   email: string;
   password: string;
   showPassword: boolean;
+  loginStatus: boolean;
 }
 
 const LoginComponent: React.FC = () => {
-  const [credentials, setCredentials] = useState<State>({
+  const [state, setState] = useState<State>({
     email: '',
     password: '',
     showPassword: false,
+    loginStatus: false,
   });
 
   const [message, setMessage] = useState<string>();
@@ -44,41 +46,31 @@ const LoginComponent: React.FC = () => {
    */
   const handleChange =
     (prop: keyof State) => (event: React.ChangeEvent<HTMLInputElement>) => {
-      setCredentials({ ...credentials, [prop]: event.target.value });
+      setState({ ...state, [prop]: event.target.value });
     };
 
   /**
    * Helper to toggle the input type of the pasword field
    */
   const handleClickShowPassword = () => {
-    setCredentials({ ...credentials, showPassword: !credentials.showPassword });
+    setState({ ...state, showPassword: !state.showPassword });
   };
 
   const handleClose = (): void => {
     setMessage('');
   };
 
-  /**
-   * DEPRECATED
-   * Passes the credentials the user has entered to the redux handler to log the user in.
-   *
-   * @param e Event that is passed in when the form is submitted
-   */
+  useEffect(() => {
+    dispatch(checkLoggedInUser());
+  }, []);
 
-  // const onSubmit = async (e: any) => {
-  //   // preventDefault prevents the page from refreshing when the form is submitted
-  //   e.preventDefault();
-  //   // simple validator for now
-  //   // TODO: Better validation for email and password
-  //   if (credentials.email && credentials.password) {
-  //     const email = credentials.email;
-  //     const password = credentials.password;
-  //     const credential = { email, password } as LoginCredentials;
-  //     dispatch(loginUser(credential));
-  //   } else {
-  //     console.log('Form is invalid');
-  //   }
-  // };
+  const loginStatus = useSelector(
+    (s: { user: { status: any } }) => s.user.status
+  );
+
+  if (loginStatus === 'succeeded') {
+    history.push('/home');
+  }
 
   const HTTPOptions: object = {
     headers: {
@@ -88,8 +80,8 @@ const LoginComponent: React.FC = () => {
 
   const onSubmit = async (e: any) => {
     e.preventDefault();
-    const email = credentials.email;
-    const password = credentials.password;
+    const email = state.email;
+    const password = state.password;
     const credential = { email, password } as LoginCredentials;
     await axios
       .post('/api/users/login', credential, HTTPOptions)
@@ -107,84 +99,86 @@ const LoginComponent: React.FC = () => {
 
   return (
     <div className="LoginComponent" data-testid="LoginComponent">
-      <Card className="form" variant="outlined">
-        <h2>Login</h2>
-        <form onSubmit={(e) => onSubmit(e)}>
-          <Grid>
-            <Grid item>
-              <FormControl fullWidth variant="filled">
-                <InputLabel className="label">Username / Email</InputLabel>
-                <Input
+      {loginStatus && loginStatus !== 'succeeded' && (
+        <Card className="form" variant="outlined">
+          <h2>Login</h2>
+          <form onSubmit={(e) => onSubmit(e)}>
+            <Grid>
+              <Grid item>
+                <FormControl fullWidth variant="filled">
+                  <InputLabel className="label">Username / Email</InputLabel>
+                  <Input
+                    fullWidth
+                    className="username"
+                    type="text"
+                    id="email"
+                    value={state.email}
+                    onChange={handleChange('email')}
+                  ></Input>
+                </FormControl>
+              </Grid>
+              <Grid item>
+                <FormControl fullWidth variant="filled">
+                  <InputLabel className="label">Password</InputLabel>
+                  <Input
+                    fullWidth
+                    className="password"
+                    type={state.showPassword ? 'text' : 'password'}
+                    id="standard-adornment-password"
+                    value={state.password}
+                    onChange={handleChange('password')}
+                    endAdornment={
+                      <InputAdornment position="end">
+                        <IconButton
+                          aria-label="toggle password visibility"
+                          onClick={handleClickShowPassword}
+                        >
+                          {state.showPassword ? (
+                            <Visibility />
+                          ) : (
+                            <VisibilityOff />
+                          )}
+                        </IconButton>
+                      </InputAdornment>
+                    }
+                  />
+                </FormControl>
+              </Grid>
+              {message && (
+                <Alert
+                  severity="error"
+                  onClose={() => handleClose()}
+                  style={{ marginTop: '10px' }}
+                  className="errorAlert"
+                >
+                  Incorrect Email or Password Combination
+                </Alert>
+              )}
+              <Grid item className="spacing">
+                <Button
                   fullWidth
-                  className="username"
-                  type="text"
-                  id="email"
-                  value={credentials.email}
-                  onChange={handleChange('email')}
-                ></Input>
-              </FormControl>
-            </Grid>
-            <Grid item>
-              <FormControl fullWidth variant="filled">
-                <InputLabel className="label">Password</InputLabel>
-                <Input
+                  className={'button'}
+                  variant="text"
+                  type="submit"
+                >
+                  Login
+                </Button>
+              </Grid>
+              <Grid item className="spacing">
+                <Button
                   fullWidth
-                  className="password"
-                  type={credentials.showPassword ? 'text' : 'password'}
-                  id="standard-adornment-password"
-                  value={credentials.password}
-                  onChange={handleChange('password')}
-                  endAdornment={
-                    <InputAdornment position="end">
-                      <IconButton
-                        aria-label="toggle password visibility"
-                        onClick={handleClickShowPassword}
-                      >
-                        {credentials.showPassword ? (
-                          <Visibility />
-                        ) : (
-                          <VisibilityOff />
-                        )}
-                      </IconButton>
-                    </InputAdornment>
-                  }
-                />
-              </FormControl>
+                  className={'button'}
+                  variant="text"
+                  type="button"
+                  onClick={() => history.push('/register')}
+                >
+                  Register
+                </Button>
+              </Grid>
             </Grid>
-            {message && (
-              <Alert
-                severity="error"
-                onClose={() => handleClose()}
-                style={{ marginTop: '10px' }}
-                className="errorAlert"
-              >
-                Incorrect Email or Password Combination
-              </Alert>
-            )}
-            <Grid item className="spacing">
-              <Button
-                fullWidth
-                className={'button'}
-                variant="text"
-                type="submit"
-              >
-                Login
-              </Button>
-            </Grid>
-            <Grid item className="spacing">
-              <Button
-                fullWidth
-                className={'button'}
-                variant="text"
-                type="button"
-                onClick={() => history.push('/register')}
-              >
-                Register
-              </Button>
-            </Grid>
-          </Grid>
-        </form>
-      </Card>
+          </form>
+        </Card>
+      )}
     </div>
   );
 };
