e5ff4e952eb13d62496cc1bfe9a232b766dfa69d
Jonathan Lee <jon.lee.jing.yu@gmail.com>
2021-06-10 18:51:09 +0800

    fix: use inheritence

--- client/src/interface/Schemas.ts
+++ client/src/interface/Schemas.ts
@@ -1,7 +1,12 @@
-export interface User {
-  name: string;
+export interface User extends PublicUser {
   email: string;
+}
+
+export interface PublicUser {
+  name: string;
+  username: string;
   avatar: string;
   bio: string;
   followCount: number;
+  followers: object;
 }
