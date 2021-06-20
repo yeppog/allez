4e0d7538b355997d874271b4da294a7f018f2136
Jonathan Lee <jon.lee.jing.yu@gmail.com>
2021-06-21 02:29:33 +0800

    feat: add desktop nav

--- client/src/components/TopNavComponent/TopNavComponent.tsx
+++ client/src/components/TopNavComponent/TopNavComponent.tsx
@@ -1,6 +1,6 @@
 import './TopNavComponent.scss';
 
-import { Button, Switch, Toolbar } from '@material-ui/core';
+import { Button, Input, Switch, Toolbar } from '@material-ui/core';
 import { Menu, Search } from '@material-ui/icons';
 import React, { useState } from 'react';
 import { logoutUser, toggleDarkMode } from './../Redux/userSlice';
@@ -18,15 +18,21 @@ const TopNavComponent: React.FC = () => {
 
   const history = useHistory();
 
+  const [search, setSearch] = useState<string>('');
+
   const loginStatus = useSelector(
     (state: { user: { status: any; darkMode: boolean } }) => state.user.status
+  );
+
+  const username = useSelector(
+    (state: { user: { user: { username: string } } }) =>
+      state.user.user.username
   );
 
   const darkMode = useSelector(
     (state: { user: { status: any; darkMode: boolean } }) => state.user.darkMode
   );
 
-  console.log(darkMode);
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     dispatch(toggleDarkMode());
   };
@@ -54,6 +60,21 @@ const TopNavComponent: React.FC = () => {
               Allez
             </NavLink>
           </Typography>
+          <div style={{ flexGrow: 1 }} className="desktopNav">
+            <Button onClick={() => history.push('/home')}>Home</Button>
+            <Button onClick={() => history.push(`/profile/${username}`)}>
+              Profile
+            </Button>
+            <Input
+              placeholder="Profile Search"
+              onChange={(e) => setSearch(e.target.value)}
+              onKeyPress={(KeyboardEvent) => {
+                if (KeyboardEvent.key == 'Enter') {
+                  history.push(`/profile/${search}`);
+                }
+              }}
+            />
+          </div>
           <Switch checked={darkMode} color="default" onChange={handleChange} />
           {loginStatus === 'succeeded' && (
             <Button type="button" onClick={logoutHandler}>
