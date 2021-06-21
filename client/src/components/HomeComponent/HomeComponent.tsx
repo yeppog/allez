ca8081edd6e3e8244e39ed81d20e2f5b8850acb2
Jonathan Lee <jon.lee.jing.yu@gmail.com>
2021-06-22 02:41:15 +0800

    feat: handle posts to map

--- client/src/components/HomeComponent/HomeComponent.tsx
+++ client/src/components/HomeComponent/HomeComponent.tsx
@@ -1,13 +1,33 @@
 import './HomeComponent.scss';
 
-import PostComponent from '../PostComponent/PostComponent';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
-const HomeComponent: React.FC = () => (
-  <div className="HomeComponent" data-testid="HomeComponent">
-    HomeComponent Component
-    <PostComponent></PostComponent>
-  </div>
-);
+import { Post } from '../../interface/Schemas';
+import PostComponent from '../PostComponent/PostComponent';
+import { fetchPosts } from '../Redux/postSlice';
+
+const HomeComponent: React.FC = () => {
+  const dispatch = useDispatch();
+  // const [postsArr, setPostsArr] = useState<Post[]>([]);
+
+  const posts = useSelector(
+    (state: { posts: { posts: { [key: string]: Post } } }) => state.posts.posts
+  );
+
+  const mappedPosts = Object.keys(posts)
+    .reverse()
+    .flatMap((key) => posts[key])
+    .map((post) => {
+      return <PostComponent key={post.id} post={post}></PostComponent>;
+    });
+
+  return (
+    <div className="HomeComponent" data-testid="HomeComponent">
+      {mappedPosts}
+      {/* <PostComponent></PostComponent> */}
+    </div>
+  );
+};
 
 export default HomeComponent;
