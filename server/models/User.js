587f805bd5d995b9083d2f64c8540134a4d610ba
Jonathan Lee <jon.lee.jing.yu@gmail.com>
2021-06-26 19:20:26 +0800

    fix: force mongo to create empty object

--- server/models/User.js
+++ server/models/User.js
@@ -4,68 +4,71 @@ const mongoose = require("mongoose");
  * Schema for the user model.
  */
 
-const UserSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-    index: true,
-    dropDups: true,
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      index: true,
+      dropDups: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      index: true,
+      dropDrups: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    activated: {
+      type: Boolean,
+    },
+    name: {
+      type: String,
+    },
+    bio: {
+      type: String,
+    },
+    avatarPath: {
+      type: String,
+    },
+    followCount: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    followers: {
+      type: [],
+      required: true,
+      default: [""],
+    },
+    followingCount: {
+      type: Number,
+      default: 0,
+    },
+    following: {
+      type: [],
+      default: [""],
+    },
+    posts: {
+      type: {},
+      default: {},
+    },
+    postCount: {
+      type: Number,
+      default: 0,
+    },
+    taggedPost: {
+      type: {},
+      default: [""],
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    index: true,
-    dropDrups: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  activated: {
-    type: Boolean,
-  },
-  name: {
-    type: String,
-  },
-  bio: {
-    type: String,
-  },
-  avatarPath: {
-    type: String,
-  },
-  followCount: {
-    type: Number,
-    required: true,
-    default: 0,
-  },
-  followers: {
-    type: [],
-    required: true,
-    default: [""],
-  },
-  followingCount: {
-    type: Number,
-    default: 0
-  },
-  following: {
-    type: [],
-    default: [""],
-  },
-  posts: {
-    type: {},
-    default: [""],
-  },
-  postCount: {
-    type: Number,
-    default: 0,
-  },
-  taggedPost: {
-    type: {},
-    default: [""],
-  },
-});
+  { minimize: false }
+);
 
 const User = mongoose.model("UserData", UserSchema);
 module.exports = User;
